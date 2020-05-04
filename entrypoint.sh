cdf3923ace44d2bce1c173aec2e6a203601ad707
Amrut Ajit Kulkarni <amrkulka@publicisgroupe.net>
2020-05-04 16:54:02 +0100

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 set -e
 git clone $GIT_SOURCE_REPO
-cd /my-first-liberty-blog/studio
-sanity init -y --create-project $PROJECT --dataset $DATASET --output-path ../temp
 cd /my-first-liberty-blog
-sed -i -e 's/"PROJECT_ID"/'$(jq .api.projectId temp/sanity.json)'/g' studio/sanity.json
-sed -i -e 's/"DATASET_ID"/'$(jq .api.dataset temp/sanity.json)'/g' studio/sanity.json
-sed -i -e 's/"PROJECT_NAME"/'$(jq .project.name temp/sanity.json)'/g' studio/sanity.json
-sed -i -e 's/"PROJECT_ID"/'$(jq .api.projectId temp/sanity.json)'/g' web/.env.production
-sed -i -e 's/"DATASET_ID"/'$(jq .api.dataset temp/sanity.json)'/g' web/.env.production
-curl -X POST -H "Authorization: Bearer $SANITY_AUTH_TOKEN"  -H "Content-Type: application/json" -d '{"origin":"https://*.netlify.app","allowCredentials":true}' https://$(jq -r .api.projectId temp/sanity.json).api.sanity.io/v1/cors
-rm -r temp
+projectid=$(curl -X POST -H "Authorization: Bearer $SANITY_AUTH_TOKEN"  -H "Content-Type: application/json" -d '{"displayName": "'$PROJECT'"}' https://api.sanity.io/v1/projects | jq -r '.id')
+sed -i -e 's/"PROJECT_ID"/"'$projectid'"/g' studio/sanity.json
+sed -i -e 's/DATASET_ID/'$DATASET'/g' studio/sanity.json
+sed -i -e 's/PROJECT_NAME/'$PROJECT'/g' studio/sanity.json
+sed -i -e 's/"PROJECT_ID"/"'$projectid'"/g' web/.env.production
+sed -i -e 's/DATASET_ID/'$DATASET'/g' web/.env.production
+curl -X POST -H "Authorization: Bearer $SANITY_AUTH_TOKEN"  -H "Content-Type: application/json" -d '{"origin":"https://*.netlify.app","allowCredentials":true}' https://$projectid.api.sanity.io/v1/cors
 git init
 git add *
 git remote set-url origin $GIT_TARGET_REPO
 git commit -m "Initial Commit"
 git push -u origin master
 cd /my-first-liberty-blog/studio
+sanity install
+sanity dataset create $DATASET --visibility public
+curl -X POST -H "Authorization: Bearer $SANITY_AUTH_TOKEN"  -H "Content-Type: application/json" -d '{"dataset": "'$DATASET'", "name": "content_update", "url": "'$WEBHOOK_ENDPOINT'"}' https://api.sanity.io/v1/hooks/projects/$projectid
 sanity dataset import production.tar.gz $DATASET
 echo "Sanity Project Provision Completed!!"
